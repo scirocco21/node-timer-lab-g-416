74f0dadf0f9c36842fb986b4a4ef2e6a445913e0
Seb Gertz <sebgertz@gmail.com>
2019-04-22 21:33:54 +0000

    Automatically backed up by Learn

--- timer.js
+++ timer.js
@@ -15,7 +15,7 @@ let duration = standardizeInput(input);
 
 while(duration > 0) {
   setInterval(function() {
-    console.log(duration);
+    console.log("Hello");
     duration -= 1;
   }, 1000);
 }
