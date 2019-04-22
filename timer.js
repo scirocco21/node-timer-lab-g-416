8751cdf2bf1729e790b8368d077ced4593c61128
Seb Gertz <sebgertz@gmail.com>
2019-04-22 21:39:28 +0000

    Automatically backed up by Learn

--- timer.js
+++ timer.js
@@ -4,7 +4,6 @@ const input = process.argv[0];
 
 function standardizeInput(input) {
   if(input[input.length - 1] === "s") {
-    console.log(input.slice(0, -1))
     return parseInt(input.slice(0, -1));
   } else if(input[input.length - 1] === "n") {
     return parseInt(input.slice(0, -3))
