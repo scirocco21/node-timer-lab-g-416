ee853490004e125327366572fb606ea6b5db5632
Seb Gertz <sebgertz@gmail.com>
2019-04-22 21:16:33 +0000

    Automatically backed up by Learn

--- timer.js
+++ timer.js
@@ -1,2 +1,20 @@
 process.argv.shift();
 process.argv.shift();
+const input = process.argv[0];
+
+function standardizeInput(input) {
+  if(input[input.length - 1] === "s") {
+    input.pop();
+    return parseInt(input);
+  } else if(input[input.length - 1] === "n") {
+    return parseInt(input.slice(0, -3))
+  }
+}
+
+const duration = standardizeInput(input);
+
+while(duration > 0) {
+  setInterval(function() {
+    console.log
+  })
+}
